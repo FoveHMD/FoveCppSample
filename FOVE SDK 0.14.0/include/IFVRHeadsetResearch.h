47240dde188dad63e43859581ac9c5dc44553a72
Jeff Bland <jeff.bland@fove-inc.com>
2018-03-12 16:03:48 +0900

    Add SDK 0.14.0 research header (no examples yet). Backwards compat is not maintained for research clients.

---
+++ FOVE SDK 0.14.0/include/IFVRHeadsetResearch.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "IFVRHeadset.h"
+#include "FoveResearchTypes.h"
+
+namespace Fove
+{
+
+class IFVRHeadsetResearch : public IFVRHeadset
+{
+public:
+	//! Register for research data, requesting what research-level data you want to receive
+	virtual EFVR_ErrorCode RegisterResearchCapabilities(EFVR_ResearchCapabilities caps) = 0;
+	virtual EFVR_ErrorCode UnregisterResearchCapabilities(EFVR_ResearchCapabilities caps) = 0;
+
+	//! Debug images from cameras
+	virtual EFVR_ErrorCode GetEyeImageData(SFVR_BitmapImage* outImage) = 0;
+	virtual EFVR_ErrorCode GetPositionImageData(SFVR_BitmapImage* outImage) = 0;
+
+	//! Returns the size in meters of the left and right pupil
+	virtual EFVR_ErrorCode GetPupilRadius(float* outLeft, float* outRight) = 0;
+};
+
+} // namespace Fove
