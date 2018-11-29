28f470acf1c7168c54130aa3998d5725cdf301cd
Jeff Bland <jeff.bland@fove-inc.com>
2018-11-29 12:22:54 -0700

    DataExample: Split error handling out into separate function, cleanup output

--- DataExample.cpp
+++ DataExample.cpp
@@ -14,6 +14,33 @@
 // Use std namespace for convenience
 using namespace std;
 
+// Helper function to check error responses from the FOVE API
+bool checkError(const Fove::ErrorCode errorCode)
+{
+	// Check for error
+	switch (errorCode) {
+	case Fove::ErrorCode::None:
+		return true;
+
+	case Fove::ErrorCode::Connect_NotConnected:
+		// This happens when the service is off, or we haven't finished connecting to it
+		cerr << "Not connected to service" << endl;
+		break;
+
+	case Fove::ErrorCode::Data_NoUpdate:
+		// This happens after we connect to the service, but before the first frame of data comes in
+		cerr << "No update" << endl;
+		break;
+
+	default:
+		// Less common errors are simply logged with their numeric value
+		cerr << "Error #" << EnumToUnderlyingValue(errorCode) << endl;
+		break;
+	}
+
+	return false;
+}
+
 int main() try {
 	// Create the Headset object, taking the capabilities we need in our program
 	// Different capabilities may enable different hardware or software, so use only the capabilities that are needed
@@ -25,34 +52,15 @@ int main() try {
 		// Fetch the left gaze vector
 		// You can swap this out with other IFVRHeadset functions to get other types of data
 		const Fove::Result<Fove::Stereo<Fove::GazeVector>> gaze = headset.getGazeVectors();
-
-		// Check for error
-		switch (gaze.getError()) {
-
-		case Fove::ErrorCode::None:
+		if (checkError(gaze.getError())) {
 			// If there was no error, we are allowed to access the other members of the struct
-			cout << "Left Gaze Vector: (" << fixed << setprecision(3)
-			     << setw(6) << gaze.getValue().l.vector.x << ", "
-			     << setw(6) << gaze.getValue().l.vector.y << ", "
-			     << setw(6) << gaze.getValue().l.vector.z << ')' << endl;
-			cout << "Right Gaze Vector: (" << fixed << setprecision(3)
-			     << setw(6) << gaze.getValue().r.vector.x << ", "
-			     << setw(6) << gaze.getValue().r.vector.y << ", "
-			     << setw(6) << gaze.getValue().r.vector.z << ')' << endl;
-			break;
-
-		case Fove::ErrorCode::Connect_NotConnected:
-			cerr << "Not connected to service" << endl;
-			break;
-
-		case Fove::ErrorCode::Data_NoUpdate:
-			cerr << "No update" << endl;
-			break;
-
-		default:
-			// Less common errors are simply logged with their numeric value
-			cerr << "Error #" << EnumToUnderlyingValue(gaze.getError()) << endl;
-			break;
+			cout << "Gaze vectors:   L(" << fixed << setprecision(3)
+			     << setw(5) << gaze.getValue().l.vector.x << ", "
+			     << setw(5) << gaze.getValue().l.vector.y << ", "
+			     << setw(5) << gaze.getValue().l.vector.z << ")   R("
+			     << setw(5) << gaze.getValue().r.vector.x << ", "
+			     << setw(5) << gaze.getValue().r.vector.y << ", "
+			     << setw(5) << gaze.getValue().r.vector.z << ')' << endl;
 		}
 
 		// Sleep for a second so that we poll at an interval instead of as much as possible
